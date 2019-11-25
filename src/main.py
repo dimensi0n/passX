85977e188c822660dcbf4b087963314cf922cc5f
MrIanis <ianis.pouru@gmail.com>
2019-11-25 08:27:04 +0000

    add main function

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from pyfiglet import Figlet
 # Shows
 def init():
     f = Figlet(font='doh')
-    print f.renderText('PassX') 
+    print(f.renderText('PassX')) 
 
 def generatePassword(stringLength=10):
     """Generate a random string of letters, digits and special characters """
@@ -13,4 +13,10 @@ def generatePassword(stringLength=10):
     password_characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(password_characters) for i in range(stringLength))
 
-init()
+def main():
+    length = int(input("How many characters do you want ?"))
+    print(generatePassword(length))
+
+if __name__ == "__main__":
+    init()
+    main()    
